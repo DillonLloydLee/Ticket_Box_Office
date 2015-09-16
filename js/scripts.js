b9b07a5eb7b787ac539645b952b3bffd7a83bbe7
Dillon Lloyd Lee <crazycapn@gmail.com>
2015-09-16 06:23:02 -0700

    Work on scripts.js and index.html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 // Ticket prototype :
 
-function Ticket(title, date, time, price) {
-  this.title = title;
-  this.date = date;
-  this.time = time;
-  this.price = price;
+function Ticket(movieTitle, buyerName, movieDate, movieTime, ticketPrice) {
+  this.movieTitle = movieTitle;
+  this.buyerName = buyerName;
+  this.movieDate = movieDate;
+  this.movieTime = movieTime;
+  this.ticketPrice = ticketPrice;
 }
 
 
@@ -17,20 +18,43 @@ var clickSound = new Audio("sounds/button.mp3");
 
 $(document).ready(function() {
 
-  $(".time").click(function() {
+  var newTime = $(".time").click(function() {
     clickSound.play();
     if ($(".age-question").css('display') == 'none') {
       $(".age-question").toggle();
     }
+    return this;
   });
 
-  $(".date").click(function() {
+  var newDate = $(".date").click(function() {
     clickSound.play();
     if ($(".movies-by-date").css('display') == 'none') {
       $(".movies-by-date").toggle();
     }
     $(".chosen-date").empty().val();
     $(".chosen-date").append($(this).clone());
+    return this;
   });
 
+  $("form#new-ticket").submit(function(event) {
+    clickSound.play();
+    event.preventDefault();
+
+    var newName = $("input.new-name").val();
+    var newAge = $("input.new-age").val();
+    var newPrice = "";
+    var newTitle = "";
+
+
+    var ticket = new Ticket(newTitle, newName, newDate, newTime, newPrice);
+    $(".new-name").append(ticket.buyerName);
+    $(".new-title").append(ticket.movieTitle);
+    $(".new-date").append(ticket.movieDate);
+    $(".new-time").append(ticket.movieTime);
+    $(".new-price").append(ticket.ticketPrice);
+
+    $(".movie-data").slideToggle();
+    $(".age-question").toggle();
+    $(".final-ticket").slideToggle();
+  });
 });
