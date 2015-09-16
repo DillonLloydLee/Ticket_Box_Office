12a54294ff156064490bd7ae7a4d99772fdaf70a
Dillon Lloyd Lee <crazycapn@gmail.com>
2015-09-16 02:27:52 -0700

    Add Ticket prototype.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 // Ticket prototype :
 
-function Ticket() {
+function Ticket(title, date, time, price) {
+  this.title = title;
+  this.date = date;
+  this.time = time;
+  this.price = price;
 }
 
 // jQuery :
